9b70851dcb0af32135e2c299962a6f579e01cb60
vinicius coppa <viniciuscoppa@gmail.com>
2023-02-22 23:11:30 -0300

    atualização do codigo, opção de jogar de novo

--- GAME 2- JavaScript/assets/scripts/javascript.js
+++ GAME 2- JavaScript/assets/scripts/javascript.js
@@ -1,56 +1,80 @@
-  let Timer = document.querySelector('.timer')
-  let trys = document.querySelector('.trys')
-  const writeInput = document.querySelector('#writeTry')
-  const buttonTry = document.querySelector('#btnTry')
-  const buttonStart = document.querySelector('#btnStart')
+let Timer = document.querySelector('.timer')
+let trys = document.querySelector('.trys')
+const writeInput = document.querySelector('#writeTry')
+const buttonTry = document.querySelector('#btnTry')
+const buttonStart = document.querySelector('#btnStart')
 
-  let numero
-  let tentativas = 10
-  let segundos = 60
-  let cron
-  let controller = 0
+let numero
+let tentativas = 10
+let segundos = 60
+let cron
+let controller = 0
 
-  buttonStart.addEventListener('click', () =>{
-    if(controller == 0){
-      numero = parseInt(Math.random() * 1000 + 1)
-      controller = 1
-      cron = setInterval(() =>{
-        if(segundos > 0){
-          segundos--
-          if(segundos == 0){
-            alert('Tempo acabou!')
-            clearInterval(cron)
-          }else if(tentativas == 0){
-            alert('tentativas acabaram')
+buttonStart.addEventListener('click', () =>{
+  if(controller == 0){
+    numero = parseInt(Math.random() * 1000 + 1)
+    controller = 1
+    
+    cron = setInterval(() =>{
+      if(segundos > 0){
+        segundos--
+        
+        if(segundos == 0){
+          alert('Tempo acabou!')
+          clearInterval(cron)
+          buttonStart.value = 'Jogar denovo'
+        }else if(tentativas == 0){
+          alert('tentativas acabaram')
           trys.innerHTML = `Tentativas: ${tentativas}`
           clearInterval(cron)
-          }
+          buttonStart.value = 'Jogar denovo' 
         }
-        Timer.innerHTML = `Tempo: ${segundos}`
-      },1000)
-    }
-  })
+        
+      }
+      
+      Timer.innerHTML = `Tempo: ${segundos}`
+    },1000)
+  }else if(buttonStart.value == 'Jogar denovo'){
+    jogardenovo()
+  }
+})
 
-  buttonTry.addEventListener('click', () =>{
-    if(controller == 1){
-      if(numero == writeInput.value && (segundos > 0 && tentativas > 0)){
-        alert('voce acertou!')
-        clearInterval(cron)
-      }else if(writeInput.value > numero && (segundos > 0 && tentativas > 0)){
-        alert('numero é menor')
-        writeInput.value = ''
-      }else if(writeInput.value < numero && (segundos > 0 && tentativas > 0)){
-        alert('numero é maior')
-        writeInput.value = ''
-      }else if(segundos == 0){
-        alert('Tempo acabou!')
-      }
-   
-      if(tentativas > 0){
-        tentativas--
-        trys.innerHTML = `Tentativas: ${tentativas}`
-      }
-    }else{
-      alert('você precisa iniciar o game antes de tentar algo!')
+buttonTry.addEventListener('click', () =>{
+  
+  if(controller == 1){
+    
+    if(numero == writeInput.value && (segundos > 0 && tentativas > 0)){
+      alert(`Você acertou o número ${numero}, jogue denovo!`)
+      clearInterval(cron)
+      buttonStart.value = 'Jogar denovo'
+    }else if(writeInput.value > numero && (segundos > 0 && tentativas > 0)){
+      alert('numero é menor')
+      writeInput.value = ''
+    }else if(writeInput.value < numero && (segundos > 0 && tentativas > 0)){
+      alert('numero é maior')
+      writeInput.value = ''
+    }else if(tentativas == 0){
+      alert('suas tentativas acabaram! Jogue denovo.')
+    }else if(segundos == 0){
+      alert('O tempo acabou! Jogue denovo.')
     }
-  })
+ 
+    if(tentativas > 0 && (writeInput != numero && segundos > 0)){
+      tentativas--
+      trys.innerHTML = `Tentativas: ${tentativas}`
+    }
+    
+  }else{
+    alert('você precisa iniciar o game antes de tentar algo!')
+  }
+  
+})
+
+function jogardenovo(){
+  controller = 0
+  segundos = 60
+  tentativas = 10
+  trys.innerHTML = `Tentativas: ${tentativas}`
+  buttonStart.value = 'Iniciar game' 
+  Timer.innerHTML = `Tempo: ${segundos}`
+}
